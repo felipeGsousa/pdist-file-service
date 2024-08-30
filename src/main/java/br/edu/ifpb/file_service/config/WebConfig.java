f1790e052d895ad579e7aacf7798241ab205bdbf
Felipe Galdino <57554381+felipeGsousa@users.noreply.github.com>
2024-08-30 02:23:48 -0300

    Add cors config

--- src/main/java/br/edu/ifpb/file_service/config/WebConfig.java
+++ src/main/java/br/edu/ifpb/file_service/config/WebConfig.java
@@ -3,11 +3,27 @@ package br.edu.ifpb.file_service.config;
 import jakarta.servlet.MultipartConfigElement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://pdist-front.vercel.app/", "https://pdist-front-felipes-projects-ed3c083c.vercel.app/", "https://pdist-front-git-main-felipes-projects-ed3c083c.vercel.app/") // Substitua pela URL do seu frontend
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://pdist-front.vercel.app/", "https://pdist-front-felipes-projects-ed3c083c.vercel.app/", "https://pdist-front-git-main-felipes-projects-ed3c083c.vercel.app/"); // Ajuste a URL conforme necessário
+            }
+        };
+    }
     @Bean
     public MultipartConfigElement multipartConfigElement() {
         return new MultipartConfigElement("", 100 * 1024 * 1024, 100 * 1024 * 1024, 0);
